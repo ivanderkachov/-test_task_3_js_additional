864fed80348fd65e337403a00d46fce4061a2425
Деркачев Иван <derkachov_ivan@host-192-168-0-103.sohonet.ua>
2022-09-22 10:06:20 +0300

    changes for deploy

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const cors = require('cors')
+const path = require("path")
+require("dotenv").config({ path: ".env" })
 const notesRouter = require('./routes/notes')
 
 const app = express()
@@ -10,9 +12,27 @@ app.use(cors())
 app.use(express.json())
 app.use('/api/v1/notes', notesRouter)
 
-app.get("/", (req, res) => {
-  res.json("Server up and running")
-})
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../build")));
+  app.get("/", (req, res) => {
+    res.sendFile(
+      path.join(__dirname, "../build", "../build/index.html"),
+      function (err) {
+        if (err) {
+          res.status(500).send(err);
+        }
+      }
+    );
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.json("Server up and running");
+  });
+}
+
+// app.get("/", (req, res) => {
+//   res.json("Server up and running")
+// })
 
 app.listen(port, () => {
   console.log(`Server has started on port ${port}`)
